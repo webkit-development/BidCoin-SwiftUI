286fee68beb0e50eabeee3534a3234ad744c302b
Kevin Stradtman <webkit.development.systems@gmail.com>
2022-04-01 00:10:10 -0400

    Initial Commit

---
+++ Bid_Coin/Bid_CoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bid_CoinApp.swift
+//  Bid_Coin
+//
+//  Created by Kevin Stradtman on 4/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Bid_CoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bid_Coin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bid_Coin
+//
+//  Created by Kevin Stradtman on 4/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
